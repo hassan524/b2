3c497dbb61521c9a5b297056d57b60af43e0e296
Hassan Rehan <hrehan752@gmail.com>
2025-05-24 12:34:07 +0500

    final

--- index.js
+++ index.js
@@ -20,8 +20,16 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', (req, res) => {
+  const userAgent = req.headers['user-agent'];
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const languages = req.headers['accept-language'];
+
+  res.json({
+    ip: ip,
+    userAgent: userAgent,
+    languages
+  });
 });
 
 // listen for requests :)
